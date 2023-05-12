5306dcda35d6365016666fee69e6ebccf3e3256d
Gabriela Caeiro <gabriela.caeiro.topa@gmail.com>
2023-05-12 02:24:55 +0200

    Add files via upload
    js added, first sounds

---
+++ app.js
@@ -0,0 +1,55 @@
+class Drumkit {
+	constructor() {
+		this.pads = document.querySelectorAll(".pad");
+		this.playBtn = document.querySelector(".play");
+		this.kickAudio = document.querySelector(".kick-sound");
+		this.snareAudio = document.querySelector(".snare-sound");
+		this.hihatAudio = document.querySelector(".hihat-sound");
+		this.index = 0;
+		this.bpm = 150;
+	}
+	activePad() {
+		this.classList.toggle("active");
+	}
+	repeat() {
+		let step = this.index % 8;
+		const activeBars = document.querySelectorAll(`.b${step}`);
+		//Loop over  pads
+		activeBars.forEach((bar) => {
+			bar.style.animation = `playTrack 0.3s alternate ease-in-out 2`;
+			//Check if pads are active
+			if (bar.classList.contains("active")) {
+				//Ckeck each sound
+				if (bar.classList.contains("kick-pad")) {
+					this.kickAudio.play();
+				}
+				if (bar.classList.contains("snare-pad")) {
+					this.snareAudio.play();
+				}
+				if (bar.classList.contains("hihat-pad")) {
+					this.hihatAudio.play();
+				}
+			}
+		});
+		this.index++;
+	}
+	start() {
+		const interval = (60 / this.bpm) * 1000;
+		setInterval(() => {
+			this.repeat();
+		}, interval);
+	}
+}
+
+const drumkit = new Drumkit();
+
+drumkit.pads.forEach((pad) => {
+	pad.addEventListener("click", drumkit.activePad);
+	pad.addEventListener("animationend", function () {
+		this.style.animation = "";
+	});
+});
+
+drumkit.playBtn.addEventListener("click", () => {
+	drumkit.start();
+});
